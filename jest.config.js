fa3abcddc5537ee5fb4deacfc2dec79be5d3252c
Satana Charuwichitratana <micksatana@live.com>
2018-12-15 23:08:44 +0700

    Move jest configuration out from package.json

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'lib/**/*.{js,jsx}',
+    '!**/node_modules/**'
+  ],
+  coverageDirectory: 'coverage',
+  roots: [
+    'test'
+  ]
+};
